dc1a357b1fddbfdfb481eebded11023f0b8d9220
Christian Matthew <87283634+ChristianMSurya@users.noreply.github.com>
2023-03-23 13:00:15 -0700

    set up button class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      classes: {
+        'button-primary': 'transition ease-in-out duration-200 group relative flex w-full py-3 justify-center rounded-md bg-indigo-600 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+      },
+    },
   },
   plugins: [],
-}
+};
