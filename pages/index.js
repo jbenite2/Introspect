5f993292bbe7c33a3d521a659e83bab20313bc45
Jose <jbenite2@nd.edu>
2023-03-29 22:58:34 -0700

    landing page

--- pages/index.js
+++ pages/index.js
@@ -1,92 +1,79 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { useState, useEffect } from 'react';
-
-export default function Home() {
-  const [data, setData] = useState([]);
-
-  const fetchData = async () => {
-    const response = await fetch('/api/users')
-    const object = await response.json()
-    setData(object)
-  }
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-
+export default function Home () {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>Introspect!</h1>
-
-        <p className={styles.description}>
-          Jose is testing the connection to the database here:
+    <div className='h-screen'>
+      <div className='h-screen flex'>
+        <div className='relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 justify-around items-center hidden'>
           <div>
-            {data.length > 0 ? (
-              data.map((item) => <p key={item.id}>{item.firstName}</p>)
-            ) : (
-              <p>No data found</p>
-            )}
+            <h1 className='text-white font-bold text-6xl font-sans'>
+              Introspect
+            </h1>
+            <h2 className='text-white text-4 font-sans'>
+              A Tech Ethics Project
+            </h2>
           </div>
-        </p>
-      </main>
+        </div>
 
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
+        <div className='flex w-1/2 justify-center items-center'>
+          <div className='w-full max-w-md space-y-8 p-10 pt-20 pb-20'>
+            <div>
+              <h2 className='text-center text-3xl font-bold tracking-tight text-gray-900'>
+                Log into your account
+              </h2>
+            </div>
+            <form className='mt-8 space-y-6' action='#' method='POST'>
+              <input type='hidden' name='remember' value='true' />
+              <div className='rounded-md shadow-sm'>
+                <div className='mb-4'>
+                  <label for='email-address' className='sr-only'>
+                    Email address
+                  </label>
+                  <input
+                    id='email-address'
+                    name='email'
+                    type='email'
+                    autocomplete='email'
+                    required
+                    className='relative block w-full pl-2 rounded-t-md border-0 py-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    placeholder='Email address'
+                  />
+                </div>
+                <div className='mb-4'>
+                  <label for='password' className='sr-only'>
+                    Password
+                  </label>
+                  <input
+                    id='password'
+                    name='password'
+                    type='password'
+                    autocomplete='current-password'
+                    required
+                    className='relative block w-full pl-2 rounded-b-md border-0 py-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    placeholder='Password'
+                  />
+                </div>
+              </div>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+              <div className=''>
+                <button
+                  type='submit'
+                  className='mb-4 btn-primary'
+                >
+                  Log in
+                </button>
+
+                <div className='flex justify-center items-center'>
+                  <a
+                    href='#'
+                    className='text-sm text-gray-600 hover:text-gray-500'
+                  >
+                Or click here to sign up
+                  </a>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
