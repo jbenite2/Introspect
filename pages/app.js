d0b78c544b3353196a5ca3c9e11f9526a6361448
Santiago <svntiiago@gmail.com>
2023-03-30 06:03:26 -0400

    setup auth

--- pages/app.js
+++ pages/app.js
@@ -18,6 +18,14 @@ export default function Home() {
                 <div className="flex w-1/2 justify-center items-center">
                     <div className="w-full max-w-md space-y-8 p-10 pt-20 pb-20">
                         <LoginBtn />
+                        <div className="flex justify-center items-center">
+                            <a
+                                href="./signup"
+                                className="text-sm text-gray-600 hover:text-gray-500"
+                            >
+                                Or click here to sign up
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
