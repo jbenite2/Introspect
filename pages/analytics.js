a3f64a7c84e8cf7f861f9423c2025de0b42fc287
Santiago <svntiiago@gmail.com>
2023-04-28 02:49:13 -0700

    loading them

--- pages/analytics.js
+++ pages/analytics.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import Navbar from './components/navbar/navbar';
-import { useSession } from 'next-auth/react';
-import UnauthorizedPage from './unauthorized';
+import React from "react";
+import Navbar from "./components/navbar/navbar";
+import { useSession } from "next-auth/react";
+import UnauthorizedPage from "./unauthorized";
 
 function AnalyticsPage() {
-  const { data: session, status } = useSession();
+    const { data: session, status } = useSession();
+    if (status == "loading") {
+        return <Spinner />;
+    }
+    if (!session || status == "unauthenticated") {
+        return <UnauthorizedPage />;
+    }
 
-	if(!session){
-		return (<UnauthorizedPage />)
-	}
+    return (
+        <div className="bg-gradient-to-tr from-purple-600 to-blue-900 min-h-screen">
+            <Navbar />
+            <div className="flex flex-col items-center justify-center h-full">
+                <div className="w-full max-w-[500px]"></div>
+                <h1 className="text-white text-4xl font-bold my-4 lg:my-8 lg:px-4 text-center">
+                    Analytics Dashboard
+                </h1>
 
-  return (
-    <div className="bg-gradient-to-tr from-purple-600 to-blue-900 min-h-screen">
-      <Navbar />
-      <div className="flex flex-col items-center justify-center h-full">
-        <div className="w-full max-w-[500px]">
+                <button className="bg-white hover:bg-gray-100 text-purple-500 font-semibold py-2 px-4 border border-purple-500 rounded shadow">
+                    Coming soon...
+                </button>
+            </div>
         </div>
-        <h1 className="text-white text-4xl font-bold my-4 lg:my-8 lg:px-4 text-center">
-          Analytics Dashboard
-        </h1>
-
-        <button className="bg-white hover:bg-gray-100 text-purple-500 font-semibold py-2 px-4 border border-purple-500 rounded shadow">
-          Coming soon...
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default AnalyticsPage;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,56 +1,65 @@
-import React from 'react';
-import Navbar from './components/navbar/navbar';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { useSession } from 'next-auth/react';
-import UnauthorizedPage from './unauthorized';
+import React from "react";
+import Navbar from "./components/navbar/navbar";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import UnauthorizedPage from "./unauthorized";
+import Spinner from "./components/spinner/spinner";
 
 function Dashboard() {
-	const { data: session, status } = useSession();
+    const { data: session, status } = useSession();
 
-	if(!session){
-		return (<UnauthorizedPage />)
-	}
+    if (status == "loading") {
+        return <Spinner />;
+    }
+    if (!session || status == "unauthenticated") {
+        return <UnauthorizedPage />;
+    }
 
-	const Router = useRouter();
-	return (
-		<div className="bg-gradient-to-tr from-purple-600 to-blue-900 min-h-screen">
-			<Navbar />
-			<div className="flex flex-col items-center justify-center h-full">
-				<div className="w-full max-w-[500px] lg:ml-[-800px]">
-					<Image
-						src="/top.png"
-						alt="Introspect"
-						width={500}
-						height={200}
-						layout="responsive"
-					/>
-				</div>
-				<h1 className="text-white text-4xl font-bold my-4 lg:my-8 lg:px-4 text-center">
-					Welcome to Introspect
-				</h1>
-				<p className="text-white ptext-lg px-10 mb-8 lg:mb-12 lg:px-4">
-					Discover insights about yourself and improve your life.
-				</p>
-				<button
-					onClick={() => Router.push('/survey/instructions')}
-					className="bg-white hover:bg-gray-100 text-purple-500 font-semibold py-2 px-4 border border-purple-500 rounded shadow"
-				>
-					Get started
-				</button>
-				<div className="w-full max-w-[500px] lg:ml-[+600px]">
-					<Image src="/bottom.png" alt="Introspect" width={500} height={200} />
-				</div>
-			</div>
-			<style jsx>{`
-				@media (max-width: 640px) {
-					.w-full {
-						display: none;
-					}
-				}
-			`}</style>
-		</div>
-	);
+    const Router = useRouter();
+    return (
+        <div className="bg-gradient-to-tr from-purple-600 to-blue-900 min-h-screen">
+            <Navbar />
+            <div className="flex flex-col items-center justify-center h-full">
+                <div className="w-full max-w-[500px] lg:ml-[-800px]">
+                    <Image
+                        src="/top.png"
+                        alt="Introspect"
+                        width={500}
+                        height={200}
+                        layout="responsive"
+                    />
+                </div>
+                <h1 className="text-white text-4xl font-bold my-4 lg:my-8 lg:px-4 text-center">
+                    Welcome to Introspect
+                </h1>
+                <p className="text-white ptext-lg px-10 mb-8 lg:mb-12 lg:px-4">
+                    Discover insights about yourself and improve your life.
+                </p>
+                <button
+                    onClick={() => Router.push("/survey/instructions")}
+                    className="bg-white hover:bg-gray-100 text-purple-500 font-semibold py-2 px-4 border border-purple-500 rounded shadow"
+                >
+                    Get started
+                </button>
+                <div className="w-full max-w-[500px] lg:ml-[+600px]">
+                    <Image
+                        src="/bottom.png"
+                        alt="Introspect"
+                        width={500}
+                        height={200}
+                    />
+                </div>
+            </div>
+            <style jsx>{`
+                @media (max-width: 640px) {
+                    .w-full {
+                        display: none;
+                    }
+                }
+            `}</style>
+        </div>
+    );
 }
 
 // export async function getServerSideProps(context) {
